1129a4d52645902437ede962fd4d84b99c257f9a
pokerflat <44443253+pokerflat@users.noreply.github.com>
2020-03-22 21:42:30 +0300

    api clents

--- js/chatView.js
+++ js/chatView.js
@@ -2,11 +2,7 @@ import { socket } from "./client.js";
 
 export function createUIMessage(msg) {
   let newMessage = document.createElement("div");
-
-  if (io.on) {
-    newMessage.setAttribute("id", "user_name_message");
-    console.log("auu");
-  }
+  newMessage.setAttribute("id", "user_name_message");
   newMessage.innerText = msg.user + ": " + msg.message.trim() + msg.file;
   document.getElementById("main_field").appendChild(newMessage);
 }
